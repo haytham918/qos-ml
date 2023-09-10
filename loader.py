73e9a613be007072f6a1958176a65e8b2a26de72
haytham918 <yunxuant@umich.edu>
2023-09-10 17:44:01 -0400

    Specify cache path and change dataset

--- loader.py
+++ loader.py
@@ -1,4 +1,5 @@
 import datasets
 from pathlib import Path
-datasets.config.DOWNLOADED_DATASETS_PATH = Path("/newdir")
-dataset = datasets.load_dataset("discofuse", "discofuse-sport")
+datasets.config.DOWNLOADED_DATASETS_PATH = Path("/newdir/download")
+datasets.config.HF_DATASETS_CACHE = Path("/newdir/cache")
+dataset = datasets.load_dataset("wikipedia", "20220301.it")
